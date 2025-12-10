39cf2bd4129f5fd1d7d982ed0eb8b3d232a3ba48
frvnzz <142498031+frvnzz@users.noreply.github.com>
2025-12-10 17:14:36 +0100

    fix: update base path in Vite configuration to relative

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ustp-cardgame-5/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
